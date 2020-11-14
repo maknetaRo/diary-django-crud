4a24cb68861b11e545621add6db4f593f9786c58
maknetaRo <makneta@gmail.com>
2020-11-14 19:17:17 +0100

    modify settings.py

--- website/settings.py
+++ website/settings.py
@@ -16,7 +16,6 @@ from decouple import config
 # Configure Django App for Heroku.
 import django_heroku
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +140,5 @@ CKEDITOR_CONFIGS = {
     }
 }
 
+
+django_heroku.settings(locals())
