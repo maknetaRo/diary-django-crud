6a7e23a10e51e69c96cd5c2bd64a6db1f769c468
maknetaRo <makneta@gmail.com>
2020-11-15 20:08:35 +0100

    database settings

--- website/settings.py
+++ website/settings.py
@@ -83,13 +83,8 @@ WSGI_APPLICATION = "website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
+DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
