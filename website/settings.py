827de06b4c02b17caef58a3e8050479517c6553e
maknetaRo <makneta@gmail.com>
2020-11-16 10:18:02 +0100

    remove materialize forms

--- website/settings.py
+++ website/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     "diary.apps.DiaryConfig",
     "ckeditor",
     "ckeditor_uploader",
-    "materializecssform",
 ]
 
 MIDDLEWARE = [
@@ -85,12 +84,14 @@ WSGI_APPLICATION = "website.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
 
 
 # Password validation
