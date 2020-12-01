d1f9b7a011bcf71ac9f1146698ef9d04e6fbe9a3
maknetaRo <makneta@gmail.com>
2020-12-01 19:31:48 +0100

    add template tags

--- diary/templatetags/diary_tags.py
+++ diary/templatetags/diary_tags.py
@@ -1,12 +1,17 @@
-# from django import template
-# from ..models import Entry
-# from django.shortcuts import get_object_or_404
+from django import template
+from ..models import Entry
+from django.shortcuts import get_object_or_404
 
-# register = template.Library()
+register = template.Library()
 
 
-# @register.inclusion_tag("diary/all_entries.html")
-# def show_all_entries():
-#     all_enteries = Entry.object.order_by("-created_on")
-#     return {"all_entries": all_enteries}
+@register.simple_tag
+def all_public_entries():
+    return Entry.objects.filter(public=True).count()
+
+
+@register.inclusion_tag("diary/all_entries.html")
+def list_all_entries():
+    all_enteries = Entry.objects.filter(public=True).order_by("-created_on")
+    return {"all_entries": all_enteries}
 
