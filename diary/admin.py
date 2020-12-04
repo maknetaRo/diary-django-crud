b564b88a41210dc921f2d65b0607cf9331213c27
maknetaRo <makneta@gmail.com>
2020-12-04 20:09:47 +0100

    added readonly fields in admin and restrected visibility of entries to public only in admin dashboard

--- diary/admin.py
+++ diary/admin.py
@@ -2,11 +2,30 @@ from django.contrib import admin
 from .models import Entry
 
 
-@admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "slug", "public")
+    readonly_fields = (
+        "author",
+        "image",
+        "title",
+        "text",
+        "public",
+    )
     list_filter = ("created_on", "author")
     raw_id_fields = ("author",)
     date_hierarchy = "created_on"
     ordering = ("-created_on",)
+
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            queryset = Entry.objects.all().filter(public=True)
+        else:
+            try:
+                queryset = Entry.objects.all().filter(author=request.user.id)
+            except:
+                queryset = Entry.objects.none()
+        return queryset
+
+
+admin.site.register(Entry, EntryAdmin)
 
--- diary/views.py
+++ diary/views.py
@@ -64,18 +64,18 @@ class EntryDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteVie
         return obj.author == self.request.user
 
 
-class CalendarView(generic.ListView):
-    model = Entry
-    template_name = "partials/_calendar.html"
-    success_url = reverse_lazy("calendar")
+# class CalendarView(generic.ListView):
+#     model = Entry
+#     template_name = "partials/_calendar.html"
+#     success_url = reverse_lazy("calendar")
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        d = get_date(self.request.GET.get("month", None))
-        cal = Calendar(d.year, d.month)
-        html_cal = cal.formatmonth(withyear=True)
-        context["calendar"] = mark_safe(html_cal)
-        context["prev_monht"] = prev_month(d)
-        context["next_month"] = next_month(d)
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         d = get_date(self.request.GET.get("month", None))
+#         cal = Calendar(d.year, d.month)
+#         html_cal = cal.formatmonth(withyear=True)
+#         context["calendar"] = mark_safe(html_cal)
+#         context["prev_monht"] = prev_month(d)
+#         context["next_month"] = next_month(d)
 
-        return context
+#         return context
